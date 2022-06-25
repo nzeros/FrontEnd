89735a73f4e85ff3b1eacd4a6acd8477f1a38020
frontendcurso <ignacio_caro93@hotmail.com>
2022-06-24 20:44:18 -0400

    actualizar7

--- js/home.js
+++ js/home.js
@@ -128,3 +128,4 @@ preguntas.forEach((pregunta) => {
 
 	});
 });
+
---
+++ js/nav.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+$(function(){
+  $("#menu").load("nav_foter/nav.html"); 
+});
+
+
+$(function(){
+  $("#foter").load("nav_foter/footer.html"); 
+});
